07ffcdab9928645d62e9d7e07488b276c1bd12da
HamzaOuamrhar <hamzaouamrhar09@gmail.com>
2023-07-10 11:58:00 +0000

    fetch products from the backend and manage state by the reducer hook

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,19 +1,54 @@
-import React from 'react'
-import data  from '../data';
+import { useEffect, useReducer, useState } from "react";
+import axios from "axios";
 
+
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 function Home() {
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    products: [],
+    loading: true,
+    error: "",
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
+    };
+    fetchData();
+  }, []);
   return (
-    <div className="products">
-        {data.products.map((product) => (
-          <div className="product" key={product.slug}>
-            <img src={product.image} alt={product.name} />
-            <p>{product.name}</p>
-            <p>{product.price}</p>
-          </div>
-        ))}
+    <div>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {loading ? (
+          <div>loading...</div>
+        ) : error ? (
+          <div>error</div>
+        ) : (
+            products.map((product) => (
+              <div>{product.price}</div>
+            ))
+        )}
       </div>
-  )
+    </div>
+  );
 }
-
-export default Home
+export default Home;
