283935edad5b99cf26375d6c57a31667824ad74c
HamzaOuamrhar <hamzaouamrhar09@gmail.com>
2023-07-25 22:52:54 +0000

    finish logic part

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import './style.css'
+import "./style.css";
 import { useContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
@@ -7,8 +7,8 @@ import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
 import Signin from "./pages/Signin";
 import { Store } from "./Store";
-import {ToastContainer} from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Shipping from "./pages/Shipping";
 import Signup from "./pages/Signup";
 import Payment from "./pages/Payment";
@@ -16,31 +16,53 @@ import PlaceOrder from "./pages/PlaceOrder";
 import Order from "./pages/Order";
 import OrderHistory from "./pages/OrderHistory";
 import Profile from "./pages/Profile";
-import Sidebar from './components/Sidebar';
-import SearchPage from './pages/SearchPage';
+import Sidebar from "./components/Sidebar";
+import SearchPage from "./pages/SearchPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  const {state} = useContext(Store)
-  const {cart} = state
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <BrowserRouter>
       <div className="App">
         <ToastContainer position="top-center" limit={1} />
-        <Header cart={cart}/>
+        <Header cart={cart} />
         <Sidebar />
         <Routes>
-          <Route path="/products/slug/:slug" element={<ProductDetail/>}/>
+          <Route path="/products/slug/:slug" element={<ProductDetail />} />
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/search" element={<SearchPage />} />
-          <Route path="/signin" element={<Signin/>} />
-          <Route path="/signup" element={<Signup/>} />
-          <Route path="/profile" element={<Profile/>} />
-          <Route path="/shipping" element={<Shipping/>} />
-          <Route path="/payment" element={<Payment/>} />
-          <Route path="/placeorder" element={<PlaceOrder/>}/>
-          <Route path="/order/:id" element={<Order/>}/>
-          <Route path="/orderhistory" element={<OrderHistory/>}/>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/shipping" element={<Shipping />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/placeorder" element={<PlaceOrder />} />
+          <Route
+            path="/order/:id"
+            element={
+              <ProtectedRoute>
+                <Order />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/orderhistory"
+            element={
+              <ProtectedRoute>
+                <OrderHistory />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </div>
     </BrowserRouter>
