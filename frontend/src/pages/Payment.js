a08c792a9dc6bb4a5b93ce37dec96d45d1988795
HamzaOuamrhar <hamzaouamrhar09@gmail.com>
2023-07-29 19:29:09 +0000

    styling payment page

--- frontend/src/pages/Payment.js
+++ frontend/src/pages/Payment.js
@@ -1,52 +1,61 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
-import {Store} from "../Store";
-import {useNavigate} from 'react-router-dom'
+import { Store } from "../Store";
+import { useNavigate } from "react-router-dom";
 
 function Payment() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const {
     cart: { shippingAddress, paymentMethod },
   } = state;
-  const [paymentMethodName, setPaymentMethodName] = useState(paymentMethod || "paypal");
-  const navigate = useNavigate()
+  const [paymentMethodName, setPaymentMethodName] = useState(
+    paymentMethod || "paypal"
+  );
+  const navigate = useNavigate();
   useEffect(() => {
-    if(!shippingAddress.address){
-      navigate('/shipping')
+    if (!shippingAddress.address) {
+      navigate("/shipping");
     }
-  }, [shippingAddress, navigate])
+  }, [shippingAddress, navigate]);
   const submitHandler = (e) => {
     e.preventDefault();
-    ctxDispatch({type: 'SAVE_PAYMENT_METHOD', payload: paymentMethodName})
-    localStorage.setItem('paymentMethod', paymentMethodName)
-    navigate('/placeorder')
+    ctxDispatch({ type: "SAVE_PAYMENT_METHOD", payload: paymentMethodName });
+    localStorage.setItem("paymentMethod", paymentMethodName);
+    navigate("/placeorder");
   };
   return (
-    <div>
+    <div className="payment">
+      <h2>Choose payment method</h2>
       <Helmet>
         <title>Payment</title>
       </Helmet>
       <form onSubmit={submitHandler}>
-        <label htmlFor="PayPal">PayPal</label>
-        <input
-          onChange={(e) => setPaymentMethodName(e.target.value)}
-          type="radio"
-          checked={paymentMethodName === "paypal"}
-          value="paypal"
-          label="PayPal"
-          id="PayPal"
-        />
-        <br />
-        <label htmlFor="Stripe">Stripe</label>
-        <input
-          onChange={(e) => setPaymentMethodName(e.target.value)}
-          type="radio"
-          checked={paymentMethodName === "stripe"}
-          value="stripe"
-          label="Stripe"
-          id="Stripe"
-        />
-        <br />
+        <div>
+          <label htmlFor="PayPal">
+            <i class="fa-brands fa-paypal"></i>
+          </label>
+          <input
+            onChange={(e) => setPaymentMethodName(e.target.value)}
+            type="radio"
+            checked={paymentMethodName === "paypal"}
+            value="paypal"
+            label="PayPal"
+            id="PayPal"
+          />
+        </div>
+        <div>
+          <label htmlFor="Stripe">
+            <i class="fa-brands fa-cc-stripe"></i>
+          </label>
+          <input
+            onChange={(e) => setPaymentMethodName(e.target.value)}
+            type="radio"
+            checked={paymentMethodName === "stripe"}
+            value="stripe"
+            label="Stripe"
+            id="Stripe"
+          />
+        </div>
         <button type="submit">Continue</button>
       </form>
     </div>
